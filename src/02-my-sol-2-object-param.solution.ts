8d6ef5d08e9af796db446c079412ad58c6f6bfd7
Simone Taeggi <startae14@gmail.com>
2023-03-21 16:32:46 +0100

    exercise Object params, Type and Interfaces

---
+++ src/02-my-sol-2-object-param.solution.ts
@@ -0,0 +1,27 @@
+import { expect, it } from "vitest";
+
+// with Type Alias definition
+type Numbers = {
+  first: number;
+  second: number;
+}
+
+export const addTwoNumbers = (params: Numbers) => {
+  return params.first + params.second;
+};
+
+it("Should add the two numbers together", () => {
+  expect(
+    addTwoNumbers({
+      first: 2,
+      second: 4,
+    }),
+  ).toEqual(6);
+
+  expect(
+    addTwoNumbers({
+      first: 10,
+      second: 20,
+    }),
+  ).toEqual(30);
+});
