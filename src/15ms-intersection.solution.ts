e4a75b55bfd8184439eab3a29447ef2d29ee0894
Simone Taeggi <startae14@gmail.com>
2023-03-22 09:52:30 +0100

    exercise Combining Types to create new Types

--- src/15ms-intersection.solution.ts
+++ src/15ms-intersection.solution.ts
@@ -14,7 +14,9 @@ interface Post {
  * How do we type this return statement so it's both
  * User AND { posts: Post[] }
  */
-export const getDefaultUserAndPosts = (): unknown => {
+type DefaultUserAndPosts = User & { posts: Post[] };
+
+export const getDefaultUserAndPosts = (): DefaultUserAndPosts => {
   return {
     id: "1",
     firstName: "Matt",
