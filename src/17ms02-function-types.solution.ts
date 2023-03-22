21c34fbb561cab3294037ed822fea3b315841d70
Simone Taeggi <startae14@gmail.com>
2023-03-22 10:20:48 +0100

    exercise Typing Functions

---
+++ src/17ms02-function-types.solution.ts
@@ -0,0 +1,21 @@
+import { Equal, Expect } from "./helpers/type-utils";
+
+type FocusListener = (isFocused: boolean) => void;
+/**
+ * How do we type onFocusChange?
+ */
+const addListener = (onFocusChange: FocusListener) => {
+  window.addEventListener("focus", () => {
+    onFocusChange(true);
+  });
+
+  window.addEventListener("blur", () => {
+    onFocusChange(false);
+  });
+};
+
+addListener((isFocused) => {
+  console.log({ isFocused });
+
+  type tests = [Expect<Equal<typeof isFocused, boolean>>];
+});
